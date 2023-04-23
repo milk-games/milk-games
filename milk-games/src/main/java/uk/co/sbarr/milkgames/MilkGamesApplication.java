a019d2732e077c3c55331a69c19ce5aae9533c0f
Scott <40842209+Sgodd@users.noreply.github.com>
2023-04-24 00:52:55 +0100

    base

---
+++ milk-games/src/main/java/uk/co/sbarr/milkgames/MilkGamesApplication.java
@@ -0,0 +1,13 @@
+package uk.co.sbarr.milkgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkGamesApplication.class, args);
+	}
+
+}
---
+++ milk-games/src/test/java/uk/co/sbarr/milkgames/MilkGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.sbarr.milkgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
