4b8615f86042fc189acaf6239ce96e4f02e3b76d
Sgodd <40842209+Sgodd@users.noreply.github.com>
2023-05-18 11:54:31 +0100

    oauth2

--- milk-games-api/src/main/java/uk/co/sbarr/milkgames/config/SecurityConfig.java
+++ milk-games-api/src/main/java/uk/co/sbarr/milkgames/config/SecurityConfig.java
@@ -1,20 +1,38 @@
 package uk.co.sbarr.milkgames.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    //     http.authorizeHttpRequests().requestMatchers("/auth*").permitAll()
+
+    //             .anyRequest().authenticated()
+
+    //             .and()
+
+    //             .oauth2Login();
+
+    //     return http.build();
+    // }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests().requestMatchers("/auth", "/auth/callback")
-                .permitAll().anyRequest().authenticated().and().oauth2Login();
+        http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/auth*", "/auth/callback", "/auth/discord/callback").permitAll()
+                .anyRequest().authenticated().and().oauth2Login().and().oauth2Client();
 
         return http.build();
     }
--- milk-games-api/src/main/java/uk/co/sbarr/milkgames/controllers/AuthController.java
+++ milk-games-api/src/main/java/uk/co/sbarr/milkgames/controllers/AuthController.java
@@ -13,29 +13,23 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-import javax.swing.text.html.FormSubmitEvent.MethodType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
@@ -44,32 +38,67 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AuthController {
 
 
+    @Autowired
     private OAuth2AuthorizedClientService authorizedClientService;
-    private OAuth2AuthorizedClientRepository clientRepository;
 
     @Autowired
-    private OAuth2AuthorizedClientManager authorizedClientManager;
+    private ClientRegistrationRepository clientRegistrationRepository;
+
 
     private HttpClient httpClient;
     private ObjectMapper objectMapper;
 
-    public AuthController(OAuth2AuthorizedClientService authorizedClientService,
-            OAuth2AuthorizedClientRepository clientRepository) {
-        this.authorizedClientService = authorizedClientService;
-        this.clientRepository = clientRepository;
-
+    public AuthController() {
         this.httpClient = HttpClient.newBuilder().version(Version.HTTP_1_1).build();
         this.objectMapper = new ObjectMapper();
     }
 
-    @RequestMapping("/callback")
-    public ResponseEntity<Void> callback(Authentication authentication,
-            HttpServletResponse response, HttpServletRequest request) throws IOException {
-        var token = clientRepository.loadAuthorizedClient("discord", authentication, request)
-                .getAccessToken();
+    @RequestMapping("/{provider}/callback")
+    public ResponseEntity<String> callback(@PathVariable String provider,
+            @RequestParam("code") String code, HttpServletResponse response) throws IOException {
+        ClientRegistration registration =
+                clientRegistrationRepository.findByRegistrationId("discord");
+
+        Map<String, String> body = new HashMap<>();
+        body.put("grant_type", registration.getAuthorizationGrantType().getValue());
+        body.put("code", code);
+        body.put("client_id", registration.getClientId());
+        body.put("client_secret", registration.getClientSecret());
+        body.put("redirect_uri", registration.getRedirectUri());
 
 
-        return ResponseEntity.ok().build();
+
+        HttpRequest request = buildRequest(registration.getProviderDetails().getTokenUri(), body);
+
+        HttpResponse<String> httpResponse = sendRequest(request);
+
+        if (httpResponse == null) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        if (httpResponse.statusCode() != 200) {
+            return ResponseEntity.status(httpResponse.statusCode()).body(httpResponse.body());
+        }
+
+        try {
+            OAuth2AccessTokenResponse tokenResponse =
+                    objectMapper.readValue(httpResponse.body(), OAuth2AccessTokenResponse.class);
+
+            /**
+             * "{
+             * "access_token": "g5N42ON4WBNiZ7fROMjmEVxhdlHNCd", 
+             * "expires_in": 604800, 
+             * "refresh_token": "6O2i5NjYsLnOGtjRJOxgrAueUzaM3w", 
+             * "scope": "identify", 
+             * "token_type": "Bearer"
+             * }"
+             */
+
+            String accessToken = tokenResponse.getAccessToken().getTokenValue();
+            return ResponseEntity.ok(accessToken);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 
@@ -79,12 +108,12 @@ public class AuthController {
     //     // ClientRegistration registration =
     //     //         clientRegistrationRepository.findByRegistrationId("discord");
 
-    //     // Map<String, String> body = new HashMap<>();
-    //     // body.put("grant_type", registration.getAuthorizationGrantType().getValue());
-    //     // body.put("code", code);
-    //     // body.put("client_id", registration.getClientId());
-    //     // body.put("client_secret", registration.getClientSecret());
-    //     // body.put("redirect_uri", registration.getRedirectUri());
+    // Map<String, String> body = new HashMap<>();
+    // body.put("grant_type", registration.getAuthorizationGrantType().getValue());
+    // body.put("code", code);
+    // body.put("client_id", registration.getClientId());
+    // body.put("client_secret", registration.getClientSecret());
+    // body.put("redirect_uri", registration.getRedirectUri());
 
     //     // HttpRequest request = buildRequest(registration.getProviderDetails().getTokenUri(), body);
 
