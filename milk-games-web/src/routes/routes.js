c17b7ac32f80a63d039c301e21a0f4e6bfb8c6c8
Sgodd <40842209+Sgodd@users.noreply.github.com>
2023-05-20 17:12:19 +0100

    landing page

--- milk-games-web/src/routes/routes.js
+++ milk-games-web/src/routes/routes.js
@@ -5,17 +5,17 @@ import Tournament from '@components/tournament/Tournament';
 import { createBrowserRouter } from 'react-router-dom';
 
 import player from './player';
+import Landing from 'Landing';
 
 export default createBrowserRouter([
   {
     path: '/',
-    element: <Auth />,
+    element: <Landing />,
   },
   {
     path: '/auth',
     com: <Auth />,
   },
-
   {
     path: '/tournament',
     element: <Auth children={<Tournament />} />,
